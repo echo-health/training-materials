c554721f43c68314547ff46092ae7c7c1483e7d0
Charlotte <charlotte.godley@echo.co.uk>
2020-02-11 15:36:05 +0000

    env from variable variant

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, %s!", os.Getenv("ENVIRONMENT"))
 }
