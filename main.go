fef0d1f3ca331cd40a598391a0d68ea1afe563ae
Charlotte <charlotte.godley@echo.co.uk>
2020-02-11 15:35:10 +0000

    env file variant

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	env, err := readVolume()
+	if err != nil {
+		fmt.Printf("Failed reading volume")
+	}
+	fmt.Fprintf(w, "Hello, %s!", env)
+}
+
+type Env struct {
+	environment string
+}
+
+func readVolume() (string, error) {
+	output, err := ioutil.ReadFile("/etc/config/envfile")
+	if err != nil {
+		return "", err
+	}
+	env := Env{}
+	err = json.Unmarshal(output, &env)
+	if err != nil {
+		return "", err
+	}
+	return env.environment, nil
 }
